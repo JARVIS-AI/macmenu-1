729588acc1a5eea36ee0d4dd65f6883a15b8a9b1
JARVIS-AI <ams.h4ck3r@gmail.com>
2022-07-17 17:37:45 +0430

    nextjs configs files

---
+++ next.sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  generateRobotsTxt: true,
+};
+
+//export default config;
